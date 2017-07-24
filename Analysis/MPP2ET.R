a0cbc812038ff5200d2efe3130d804dbe80074de
clee9714 <clee69@u.rochester.edu>
2017-07-24 13:12:59 -0400

    analysis

--- Analysis/MPP2ET.R
+++ Analysis/MPP2ET.R
@@ -21,7 +21,9 @@ setwd('/Users/crystallee/Documents/Github/MannerPathPriming-2ET/Data/Melissa_111
 path = '~/Documents/Github/MannerPathPriming-2ET/Data/Melissa_111'
 out.file<-""
 
-#Load in practice trials
+############################
+# LOOKING AT PRACTICE TRIALS
+############################
 file.names <- dir(path, pattern ="gaze_MPPCREATION_Melissa_111_All_of_Practice_.*.csv")
 df_111_practice <- data.frame(Date=as.Date(character()),
                              File=character(), 
@@ -37,18 +39,84 @@ dat_table <- read.delim("~/Documents/Github/MannerPathPriming-2ET/Data/MPPCREATI
            header=TRUE, sep=",")
 
 #cleaning up the data to get it in the form I want
-colnames(df_111_practice)[which(names(df_111_practice) == "description")] <- "Trial"
+colnames(df_111_practice)[which(names(df_111_practice) == "description")] <- "trialNo"
 df_111_practice$L_valid <- as.factor(df_111_practice$L_valid)
 df_111_practice$R_valid <- as.factor(df_111_practice$R_valid)
 df_111_practice$system_time_stamp <- df_111_practice$system_time_stamp - 1500000000000000
 
 #defining a trackloss column
-df_111_practice$Trackloss_column <- as.factor(ifelse(df_111_practice$L_valid == '1' & df_111_practice$R_valid == '1', TRUE, 
-                                             ifelse(df_111_practice$L_valid == '0' & df_111_practice$R_valid == '1', FALSE,
-                                             ifelse(df_111_practice$L_valid == '1' & df_111_practice$R_valid == '0', FALSE,
-                                             ifelse(df_111_practice$L_valid == '0' & df_111_practice$R_valid == '0', FALSE, 'Error')))))
+df_111_practice$Trackloss_column <- ifelse(df_111_practice$L_valid == '1' & df_111_practice$R_valid == '1', FALSE, 
+                                           ifelse(df_111_practice$L_valid == '0' & df_111_practice$R_valid == '1', TRUE,
+                                                  ifelse(df_111_practice$L_valid == '1' & df_111_practice$R_valid == '0', TRUE,
+                                                         ifelse(df_111_practice$L_valid == '0' & df_111_practice$R_valid == '0', TRUE, 'Error'))))
+df_111_practice$Trackloss_column <- as.logical(df_111_practice$Trackloss_column)
 
-#reading in main trial CSVs
+#read in timestamps
+df_timestamps <- read.csv("~/Documents/Github/MannerPathPriming-2ET/Data/Melissa_111/timestamps_MPPCREATION_Melissa_111.csv", header = TRUE, stringsAsFactors=FALSE, fileEncoding = "latin1")
+df_timestamps$subjectID <- as.factor(df_timestamps$subjectID)
+df_timestamps$system_time_stamp <- df_timestamps[,2] - 1500000000000000
+
+#applying it to the dataframe for practice trials
+a <- lapply(df_111_practice$system_time_stamp, trial_time)
+df_111_practice$Trial_description <- a
+
+#averaging together L and R eyes
+df_111_practice$X <- rowMeans(subset(df_111_practice, select = c(6, 9)), na.rm = TRUE)
+df_111_practice$Y <- rowMeans(subset(df_111_practice, select = c(7, 10)), na.rm = TRUE)
+
+#merging together dat_table and trials to get correctness
+df_111_practice$trialNo <- as.factor(ifelse(df_111_practice$trialNo == "All_of_Practice_trial_1", "1", 
+                                        ifelse(df_111_practice$trialNo == "All_of_Practice_trial_2", "2",
+                                               ifelse(df_111_practice$trialNo == "All_of_Practice_trial_3", "3",
+                                                      ifelse(df_111_practice$trialNo == "All_of_Practice_trial_4", "4", "Error")))))
+df_111_practice <- merge(df_111_practice, dat_table, by="trialNo")
+
+#Making sure they're looking at the correct video or not
+subjID_aoi <- read.csv("~/Documents/Github/MannerPathPriming-2ET/Analysis/subjID_aoi.csv", header = TRUE, stringsAsFactors=FALSE, fileEncoding="latin1")
+subjID_aoi$trialNo <- as.factor(subjID_aoi$trialNo)
+df_111_main_aoi <- add_aoi(df_111_main, aoi_dataframe = subjID_aoi, 'X', 'Y', aoi_name="Correct", x_min_col = "X_min",
+                           x_max_col = "X_max", y_min_col = "Y_min", y_max_col = "Y_max")
+
+#adding an AOI column for Incorrect looks to screen
+df_111_practice$Incorrect <- ifelse(df_111_practice$Correct == TRUE, FALSE,
+                                    ifelse(df_111_practice$Correct == FALSE, TRUE, 'Error'))
+df_111_practice$Incorrect <- as.logical(df_111_practice$Incorrect)
+
+#starting to use eyetrackingR
+data <- make_eyetrackingr_data(df_111_practice_aoi, 
+                               participant_column = "subjectID",
+                               trial_column = "trialNo",
+                               time_column = "system_time_stamp",
+                               trackloss_column = "Trackloss_column",
+                               aoi_columns = c("Correct", "Incorrect"),
+                               treat_non_aoi_looks_as_missing = TRUE
+)
+
+#aggregating by subjectID to get a proportion of looks to screen by AOI, with only trial 5
+trial5 <- subset(data, trialNo == "5")
+data_summary <- describe_data(trial5, 
+                              describe_column='Correct', group_columns=c('subjectID'))
+response_window_agg_by_sub <- make_time_window_data(trial5, aois = c("Correct", "Incorrect"), summarize_by = "subjectID")
+
+#creating plots
+ggplot(data=response_window_agg_by_sub, aes(x=AOI, y=Prop)) +
+  geom_bar(stat="identity", position=position_dodge(), colour="black") + 
+  ylab("Proportion of looks to screen") +
+  ggtitle("Path Condition") +
+  theme(axis.title = element_text(size=18),
+        axis.text.x  = element_text(size=18),
+        axis.text.y = element_text(size=18),
+        plot.title = element_text(size=18, face="bold")) +
+  scale_x_discrete(breaks=c("Correct", "Incorrect"),
+                   labels=c("Path", "Manner"))
+ggsave("trial5_melissa_path.png")
+
+
+
+############################
+# LOOKING AT MAIN TRIALS
+############################
+
 file.names <- dir(path, pattern ="gaze_MPPCREATION_Melissa_111_All_of_Main_trial_.*.csv")
 df_111_main <- data.frame(Date=as.Date(character()),
                               File=character(), 
@@ -77,11 +145,29 @@ df_timestamps <- read.csv("~/Documents/Github/MannerPathPriming-2ET/Data/Melissa
 df_timestamps$subjectID <- as.factor(df_timestamps$subjectID)
 df_timestamps$system_time_stamp <- df_timestamps[,2] - 1500000000000000
 
-# me practicing
-practice1 <- df_111_main[1,]
-practice2 <- df_111_main
+#applying it to the dataframe for main trials
+a <- lapply(df_111_main$system_time_stamp, trial_time)
+df_111_main$Trial_description <- a
 
-#declaring my real function
+#averaging together L and R eyes
+df_111_main$X <- rowMeans(subset(df_111_main, select = c(6, 9)), na.rm = TRUE)
+df_111_main$Y <- rowMeans(subset(df_111_main, select = c(7, 10)), na.rm = TRUE)
+
+#merging together dat_table and trials to get correctness
+df_111_main$trialNo <- as.factor(ifelse(df_111_main$trialNo == "All_of_Main_trial_5", "5", 
+                                        ifelse(df_111_main$trialNo == "All_of_Main_trial_6", "6",
+                                               ifelse(df_111_main$trialNo == "All_of_Main_trial_7", "7",
+                                                      ifelse(df_111_main$trialNo == "All_of_Main_trial_8", "8", "Error")))))
+df_111_main <- merge(df_111_main, dat_table, by="trialNo")
+
+#Making sure they're looking at the correct video or not
+subjID_aoi <- read.csv("~/Documents/Github/MannerPathPriming-2ET/Analysis/subjID_aoi.csv", header = TRUE, stringsAsFactors=FALSE, fileEncoding="latin1")
+subjID_aoi$trialNo <- as.factor(subjID_aoi$trialNo)
+df_111_main_aoi <- add_aoi(df_111_main, aoi_dataframe = subjID_aoi, 'X', 'Y', aoi_name="Correct", x_min_col = "X_min",
+                           x_max_col = "X_max", y_min_col = "Y_min", y_max_col = "Y_max")
+
+
+###declaring my real function####
 trial_time <- function(x) {
 
   f = df_timestamps$system_time_stamp
@@ -173,6 +259,10 @@ ggplot(data=response_window_agg_by_sub, aes(x=AOI, y=Prop)) +
   geom_bar(stat="identity", position=position_dodge(), colour="black") + 
   ylab("Proportion of looks to screen") +
   ggtitle("Path Condition") +
+  theme(axis.title = element_text(size=18),
+        axis.text.x  = element_text(size=18),
+        axis.text.y = element_text(size=18),
+        plot.title = element_text(size=18, face="bold")) +
   scale_x_discrete(breaks=c("Correct", "Incorrect"),
                    labels=c("Path", "Manner"))
 ggsave("trial5_melissa_path.png")
@@ -260,6 +350,10 @@ ggplot(data=response_window_agg_by_sub, aes(x=AOI, y=Prop)) +
   geom_bar(stat="identity", position=position_dodge(), colour="black") + 
   ylab("Proportion of looks to screen") +
   ggtitle("Path Condition") +
+  theme(axis.title = element_text(size=18),
+        axis.text.x  = element_text(size=18),
+        axis.text.y = element_text(size=18),
+        plot.title = element_text(size=18, face="bold")) +
   scale_x_discrete(breaks=c("Correct", "Incorrect"),
                    labels=c("Outcome", "Action"))
 ggsave("extendtrials_melissa_path.png")
